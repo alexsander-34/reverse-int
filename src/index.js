287ffaf820a1f1529c35e605a933a89f12b45353
Alexsander Brantsevich <lexsander.red.34@gmail.com>
2020-10-25 22:58:36 +0300

    add: code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function reverse (n) {
-  
+    const numb = Math.abs(n);
+    const string = String(numb);
+    const reverse = string.split('').reverse().join('');
+    return  Number(reverse);
 }
